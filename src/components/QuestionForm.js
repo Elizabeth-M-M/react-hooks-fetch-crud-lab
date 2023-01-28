2820c8d6582deeab5fe3f60bf4b3527ba3bb0d7f
Elizabeth <mwendemuthusi6@gmail.com>
2023-01-28 13:38:08 +0300

    completed assignment

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 function QuestionForm(props) {
-  const [formData, setFormData] = useState({
+ const [formData, setFormData] = useState({
     prompt: "",
     answer1: "",
     answer2: "",
@@ -19,9 +19,30 @@ function QuestionForm(props) {
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(formData);
+const itemData ={
+      prompt:formData.prompt,
+      answers: [formData.answer1, formData.answer2, formData.answer3, formData.answer4],
+      correctIndex: formData.correctIndex
+    }
+    // console.log(itemData)
+    fetch('http://localhost:4000/questions', {
+      method:'POST',
+      headers:{
+        'Content-Type':"application/json"
+      },
+      body:JSON.stringify(itemData)
+    }).then(r=>r.json())
+    .then(data=>props.onAddQuestion(data))
+    setFormData({
+    prompt: "",
+    answer1: "",
+    answer2: "",
+    answer3: "",
+    answer4: "",
+    correctIndex: 0,
+  })
   }
-
+// console.log(formData)
   return (
     <section>
       <h1>New Question</h1>
